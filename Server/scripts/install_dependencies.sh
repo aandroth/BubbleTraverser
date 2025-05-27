ad179d34187bd2e4b0d132ae474451915cbf6a4d
aandroth <aandroth@gmail.com>
2025-05-27 01:28:21 -0700

    Added back the nvm and npm install

--- Server/scripts/install_dependencies.sh
+++ Server/scripts/install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16.0.0
