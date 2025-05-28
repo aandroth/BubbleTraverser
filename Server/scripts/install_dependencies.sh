9dc0a2a0837c17974d3b1dee2bc8411e0aa29902
aandroth <aandroth@gmail.com>
2025-05-27 21:49:47 -0700

    Corrected whch to which

--- Server/scripts/install_dependencies.sh
+++ Server/scripts/install_dependencies.sh
@@ -4,5 +4,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16.0.0
-sudo cp $(whch npm) /usr/bin/npm
-sudo cp $(whch node) /usr/bin/node
+sudo cp $(which npm) /usr/bin/npm
+sudo cp $(which node) /usr/bin/node
